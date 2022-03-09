a9c513a098bc78b279adcdab2a9fd1a065d600f6
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-03-09 16:37:39 +0100

    Replace mock data with resolvers

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,29 +1,9 @@
 //TODO
 import { ApolloServer } from 'apollo-server'
 import { typeDefs } from './schema.mjs'
+import { resolvers } from './resolvers.mjs'
 
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(8)]
-  }),
-  Track: () => ({
-    id: () => 'track_01',
-    title: () => 'Astro Kitty, Space Explorer',
-    author: () => {
-      return {
-        name: 'Grumpy Cat',
-        photo:
-          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
-      };
-    },
-    thumbnail: () =>
-      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-}
-
-const server = new ApolloServer({ typeDefs, mocks: mocks })
+const server = new ApolloServer({ typeDefs, resolvers })
 // console.log(Object.keys(server), ': server keys')
 
 server.listen().then(({ port }) => {
