915ad668c7d29175e3dda94d57b88981603a5f76
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-03-09 14:04:05 +0100

    Add apollos useQuery hook

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Layout } from '../components';
-import { gql } from '@apollo/client'
+import { Layout, QueryResult } from '../components';
+import { useQuery, gql } from '@apollo/client'
+import TrackCard from '../containers/track-card';
 
 export const TRACKS = gql`
   query ExampleQuery {
@@ -24,7 +25,17 @@ export const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, data, error } = useQuery(TRACKS)
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
