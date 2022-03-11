f163e7364b8785d8d12345975752a42570f49d79
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-03-11 14:14:29 +0100

    Add commment and formatting to schema

--- server/src/schema.mjs
+++ server/src/schema.mjs
@@ -1,26 +1,31 @@
-import { gql } from 'apollo-server'
+import { gql } from 'apollo-server';
+
+// This file defines the fields that are chosen to be used in the query
+// There are additional fields available on the Track object
+// See https://odyssey-lift-off-rest-api.herokuapp.com/docs/
+// for more details
 
 const typeDefs = gql`
-  type Query {
-    tracksForHome: [Track!]!
-  }
+    type Query {
+        tracksForHome: [Track!]!
+    }
 
-"A track is a group of Modules that teaches about a specific topic"
-  type Track {
-    id: ID!
-    title: String!
-    author: Author!
-    thumbnail: String
-    length: Int
-    modulesCount: Int
-  }
+    "A track is a group of Modules that teaches about a specific topic"
+    type Track {
+        id: ID!
+        title: String!
+        author: Author!
+        thumbnail: String
+        length: Int
+        modulesCount: Int
+    }
 
-  "Author of a complete track"
-  type Author {
-    id: ID!
-    name: String!
-    photo: String
-  }
-`
+    "Author of a complete track"
+    type Author {
+        id: ID!
+        name: String!
+        photo: String
+    }
+`;
 
-export { typeDefs }
+export { typeDefs };
