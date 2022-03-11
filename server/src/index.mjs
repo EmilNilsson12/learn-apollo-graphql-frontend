fb7915edd026d9158d0f632cbdc576d01ebbbb28
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-03-11 14:14:49 +0100

    Formating index

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,18 +1,20 @@
 //TODO
-import { ApolloServer } from 'apollo-server'
-import { typeDefs } from './schema.mjs'
-import { resolvers } from './resolvers.mjs'
-import { TrackAPI } from './datasources/track-api.mjs'
+import { ApolloServer } from 'apollo-server';
+import { typeDefs } from './schema.mjs';
+import { resolvers } from './resolvers.mjs';
+import { TrackAPI } from './datasources/track-api.mjs';
 
-const server = new ApolloServer({ 
-  typeDefs, 
-  resolvers, 
-  dataSources: () => {
-    return {
-      trackAPI: new TrackAPI 
-    }
-  }
-})
+// ApolloServer takes an object as argument
+// called Constructor object
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => {
+        return {
+            trackAPI: new TrackAPI(),
+        };
+    },
+});
 // console.log(Object.keys(server), ': server keys')
 
 server.listen().then(({ port }) => {
@@ -20,6 +22,5 @@ server.listen().then(({ port }) => {
     🚀  Server is running!
     🔉  Listening on port: ${port}
     📭  Query at https://studio.apollographql.com/dev
-    `)
-  }
-)
+    `);
+});
