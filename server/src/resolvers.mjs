4a259b37f9d558f549345e14bb46538528d2453e
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-03-11 12:45:48 +0100

    Add comments explaining resolvers

--- server/src/resolvers.mjs
+++ server/src/resolvers.mjs
@@ -1,10 +1,19 @@
+// resolver structure
+// key: (parent, args, context, info) => {},
+// parent desctructured
+// args omitted and denoted with __
+// context destructured
+// info omitted completely
+
 const resolvers = {
-  Query: {
-    tracksForHome: (_, __, { dataSources }) => dataSources.trackAPI.getTracksForHome()
-  },
-  Track: {
-    author: ({ authorId }, __, { dataSources }) => dataSources.trackAPI.getAuthor(authorId)
-  }
+    Query: {
+        tracksForHome: (_, __, { dataSources }) =>
+            dataSources.trackAPI.getTracksForHome(),
+    },
+    Track: {
+        author: ({ authorId }, __, { dataSources }) =>
+            dataSources.trackAPI.getAuthor(authorId),
+    },
 };
 
-export { resolvers }
+export { resolvers };
