726e835959a51df952fd51054b80becc2d495160
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-03-12 14:48:48 +0100

    Add track endpoint to trackAPI

--- server/src/datasources/track-api.mjs
+++ server/src/datasources/track-api.mjs
@@ -1,17 +1,21 @@
-import { RESTDataSource } from "apollo-datasource-rest";
+import { RESTDataSource } from 'apollo-datasource-rest';
 
 class TrackAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
   }
 
   getTracksForHome() {
-    return this.get("tracks");
+    return this.get('tracks');
   }
 
   getAuthor(authorId) {
     return this.get(`/author/${authorId}`);
+  }
+
+  getTrack(trackId) {
+    return this.get(`/track/${trackId}`);
   }
 }
 
