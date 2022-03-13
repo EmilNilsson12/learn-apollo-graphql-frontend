356709e3004e4d6b4e661480c61a54b94626e0da
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-03-13 15:15:36 +0100

    Add modules query and page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import Pages from './pages';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://catstronauts-eminil12.herokuapp.com/',
+  // uri: 'https://catstronauts-eminil12.herokuapp.com/',
+  uri: 'http://localhost:4000/',
   cache: new InMemoryCache(),
 });
 
