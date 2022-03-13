20bd09c2929aae607553c6d5b547bf02691b2dd3
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2022-03-13 12:54:39 +0100

    Add deploy script

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GlobalStyles from "./styles";
-import Pages from "./pages";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyles from './styles';
+import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: 'https://catstronauts-eminil12.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
@@ -16,5 +16,5 @@ ReactDOM.render(
       <Pages />
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
